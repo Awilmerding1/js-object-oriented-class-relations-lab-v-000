f29a71ceb6ad9377e7d715d9ff18951d895f8247
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 20:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,12 +55,12 @@ class Trip {
     store.trips.push(this)
   }
   
-  setDriver(driver){
-	  this.driverId = driver.id
-	}
-  setPassenger(passenger){
-	  this.passengerId = passenger.id
-	}
+//   setDriver(driver){
+// 	  this.driverId = driver.id
+// 	}
+//   setPassenger(passenger){
+// 	  this.passengerId = passenger.id
+// 	}
 
   driver() {
     return store.drivers.find(function(driver){
