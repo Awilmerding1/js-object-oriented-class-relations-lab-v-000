07905f2351136a3bcf8a8d0d1df6a15b65df6dbd
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 20:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ let userId = 0
 let store = {drivers: [], passengers: [], trips: []}
 
 class Driver {
-  
   constructor(name) {
     this.name = name
     this.id = ++ userId
     store.drivers.push(this)
   }
-  
-  
-  
 }
